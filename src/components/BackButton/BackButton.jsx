d6ef81f09235bdab4c669d1cea6deb6aac5037d9
tekostyshyn <tekostyshyn@gmail.com>
2023-06-04 21:52:27 +0300

    styles finished

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,18 +1,18 @@
 import { FiArrowLeft } from 'react-icons/fi';
-import { Button } from './BackButton.styled';
+import { NavLink } from './BackButton.styled';
 
 const BackButton = () => {
   return (
-    <Button>
-      <FiArrowLeft
-        style={{
-          width: '24px',
-          height: '24px',
-          color: '#471ca9',
-        }}
-      />
-      Go back
-    </Button>
+    <NavLink to={'/'}>
+        <FiArrowLeft
+          style={{
+            width: '24px',
+            height: '24px',
+            color: '#471ca9',
+          }}
+        />
+        Go back
+    </NavLink>
   );
 };
 
--- src/components/BackButton/BackButton.styled.js
+++ src/components/BackButton/BackButton.styled.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const Button = styled.button`
-  width: 180px;
-  height: 40px;
-  background-color: transparent;
-  border: none;
+export const NavLink = styled(Link)`
   display: flex;
   align-items: center;
   gap: 5px;
   font-size: 18px;
+  font-weight: 500;
+  text-decoration: none;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+  }
 `;
